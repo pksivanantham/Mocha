8ed9f86901a4432d012da832883fb5a93459cdd4
Sivanantham Padikkasu <pksivanantham@gmail.com>
2017-09-17 00:02:47 +0530

    should assertation added

--- test.js
+++ test.js
@@ -1,11 +1,56 @@
 var assert = require('assert');
-var expect=require('chai').expect;
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+var chai =require('chai');
+var expect=chai.expect;
+var should=chai.should();
+
+
+describe('Array Test cases', function() {
+
+  var arrayOfNumbers=[];
+
+    before(function() {
+
+    arrayOfNumbers=[1,2,3]
+      // runs before all tests in this block
+    });
+  
+    after(function() {
+    
+      // runs after all tests in this block
+    });
+  
+    beforeEach(function() {
+    
+      // runs before each test in this block
+    });
+  
+    afterEach('after each hooks calling',function() {
+    
+      // runs after each test in this block
+    });
+  
+    // test cases
+ 
+  it('array must contains more than one value', function () {
+
+    expect(arrayOfNumbers).to.have.lengthOf(3);
+
+    arrayOfNumbers.should.have.lengthOf(3);
+  });
+
+    //This one consider as pending test because it does not have a call back
+    it('The array length always greater than 0');
+
+    describe('#index of ',function(){
+
+      it('should return -1 when the value is not present', function() {
+        assert.equal(-1, arrayOfNumbers.indexOf(4));
+      });
+  
+      it('should return index when the value is not present', function() {
+        assert.equal(0, arrayOfNumbers.indexOf(1));
+      });
+  
     });
   });
-});
-
 
